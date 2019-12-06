ad4f8d93555c286940bcbf09d9c8d1114936b235
jianxg <419533848@qq.com>
2019-12-06 13:56:10 +0800

    message:commit

--- src/main/java/com/example/springbootdemo/controller/EmpController.java
+++ src/main/java/com/example/springbootdemo/controller/EmpController.java
@@ -11,8 +11,41 @@ public class EmpController {
     private JdbcTemplate jdbc;
 
     @RequestMapping(value = "/getDate")
-    public Object get(int id){
+    public Object get(int id)
+    {
         String sql = "SELECT * FROM \"Emp\" where \"Id\"=?";
         return jdbc.queryForList(sql, id);
+    }
+
+    @RequestMapping(value = "/add")
+    public Object add(String name)
+    {
+        String sql = "INSERT INTO \"Emp\"(\"Name\") VALUES (?);";
+        jdbc.update(sql,name);
+        return "新增成功";
+    }
+
+    @RequestMapping(value = "/update")
+    public Object update(int id ,String name)
+    {
+        String sql = "UPDATE \"Emp\" SET \"Name\"=? WHERE \"Id\"=?";
+        System.err.println("delete");
+        jdbc.update(sql,name,id);
+        return "更新成功！";
+    }
+
+    @RequestMapping(value = "/delete")
+    public Object delete(int id){
+        String sql = "DELETE FROM \"Emp\" WHERE \"Id\"=?";
+        jdbc.update(sql,id);
+        return "删除成功！";
+    }
+
+    @RequestMapping(value = "/count")
+    public Integer count( )
+    {
+        String sql = "SELECT count(*) FROM \"Emp\"  ;";
+        Integer k=jdbc.queryForObject(sql, Integer.class);
+        return	k;
     }
 }
